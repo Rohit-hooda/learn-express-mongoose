5f5e2cc607ea00476d8db4102ade30d2e30eb36c
Rohit Hooda <rohithooda1999@gmail.com>
2024-03-15 23:05:49 -0400

    Added the classs activity

---
+++ models/genre.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema(
+  {
+    name: {type: String, required: true},
+    url: {type: String, required: true},
+}
+);
+
+
+//Export model
+module.exports = mongoose.model('Genre', GenreSchema);
